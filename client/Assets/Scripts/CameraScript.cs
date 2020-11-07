8717d651716ceb809c30b52b049eeaa400310788
Đorđe Cvijić <djordje.cvijic.etf@gmail.com>
2020-11-07 18:21:19 +0100

    fix krz

--- client/Assets/Scripts/CameraScript.cs
+++ client/Assets/Scripts/CameraScript.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
@@ -8,22 +6,16 @@ public class CameraScript : MonoBehaviour
     public float smoothTime = 0.3F;
     public Vector3 followDistance = new Vector3(0, 1f, -1f);
 
-    private Vector3 velocity = Vector3.zero;
-
     void LateUpdate()
     {
+        // rotate
         transform.rotation = target.rotation;
 
         // Define a target position above and behind the target transform
         var relativeFollow = followDistance.x * target.right + followDistance.y * target.up + followDistance.z * target.forward;
-        Vector3 targetPosition = target.transform.position + relativeFollow;
+        var targetPosition = target.transform.position + relativeFollow;
 
         // Smoothly move the camera towards that target position
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-    }
-
-    void Start() 
-    {
-
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothTime);
     }
 }
--- client/Assets/Scripts/SpectatorScript.cs
+++ client/Assets/Scripts/SpectatorScript.cs
@@ -5,16 +5,8 @@ public class SpectatorScript : MonoBehaviour
     public float maxSpeed;
     public float acceleration;
     public float speedFallOff = 0.5f;
-    public float smoothTime = 0.3f;
 
     private Vector3 currentSpeed = Vector3.zero;
-    private Vector3 targetPosition;
-    private Vector3 smoothCurrentVelocity = Vector3.zero;
-    
-    private void Start()
-    {
-        targetPosition = transform.position;
-    }
 
     private void Update()
     {
@@ -34,7 +26,6 @@ public class SpectatorScript : MonoBehaviour
         Vector3.ClampMagnitude(currentSpeed, maxSpeed);
 
         // move
-        targetPosition += currentSpeed;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref smoothCurrentVelocity, smoothTime);
+        transform.position += currentSpeed;
     }
 }
