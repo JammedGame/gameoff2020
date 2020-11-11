8e5d205f742b605c142f91dc3e3db25c45ff06e7
Dusan Cvijic <dusancvijic1906@gmail.com>
2020-11-11 23:18:47 +0100

    Added Main Menu scene as well as simple main menu controls (incomplete)

---
+++ client/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Communication;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public enum MainMenuItem 
+    {
+        Main,
+        Lobby,
+        Options,
+        Credits
+    }
+
+    public GameObject mainMenu;
+    public GameObject lobby;
+    public GameObject options;
+    public GameObject credits;
+
+    public GameObject exitButton;
+
+    void Start()
+    {
+        #if UNITY_WEBGL && !UNITY_EDITOR
+            exitButton.SetActive(false);
+        #endif
+    }
+
+    public void CreateGame()
+    {
+        var DebugGameName = "dusantest";
+
+        // create game
+        MoonshotServer.Instance.CreateGame(new CreateGameRequest
+        {
+            name = DebugGameName,
+        }, response =>
+        {
+            var jsonResponse = JsonUtility.ToJson(response, true);
+            Debug.Log($"{nameof(CreateGame)} : {jsonResponse}");
+        });
+    }
+
+    public void ClickStartGame()
+    {
+        mainMenu.SetActive(false);
+        lobby.SetActive(true);
+
+        CreateGame();
+    }
+
+    public void ClickOptions()
+    {
+        mainMenu.SetActive(false);
+        options.SetActive(true);
+    }
+    public void ClickCredits()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(true);
+    }
+    public void ClickExit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif !UNITY_WEBGL
+            Application.Quit();
+        #endif
+    }
+}
