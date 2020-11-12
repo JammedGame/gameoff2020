91fe817a88842d2b8e2649f347a05e354d3dd257
Dusan Cvijic <dusancvijic1906@gmail.com>
2020-11-13 00:28:49 +0100

    progress with lobby, nearly done setting up listing games in lobby

---
+++ client/Assets/Scripts/LobbyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Communication;
+using UnityEngine;
+
+public class LobbyController : MonoBehaviour
+{
+
+    private readonly static List<GameSetupData> games = new List<GameSetupData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ListGames();
+    }
+
+    public void CreateGame(string gameName = "dusantest")
+    {
+        // create game
+        MoonshotServer.Instance.CreateGame(new CreateGameRequest
+        {
+            name = gameName,
+        }, response =>
+        {
+            var jsonResponse = JsonUtility.ToJson(response, true);
+            Debug.Log($"{nameof(CreateGame)} : {jsonResponse}");
+        });
+    }
+
+    public void ListGames() 
+    {
+        MoonshotServer.Instance.ListGames(new ListGamesRequest(), response =>
+        {
+            var jsonResponse = JsonUtility.ToJson(response, true);
+            // Debug.Log($"{nameof(ListGames)} : {jsonResponse}");
+            games.AddRange(response.games);
+            int i = 0;
+            int x = 50;
+            int y = 50;
+            games.ForEach((game) => {
+                var gameItem = Instantiate(Resources.Load<LobbyGameItem>("Prefabs/LobbyGameItem"));
+                gameItem.SetGameName(game.name);
+                gameItem.transform.localPosition = new Vector3(0, y + y * i, 0);
+            });
+        });
+
+    }
+}
---
+++ client/Assets/Scripts/LobbyGameItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyGameItem : MonoBehaviour
+{
+    public UnityEngine.UI.Text gameNameTextField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetGameName(string gameName)
+    {
+        gameNameTextField.text = gameName;
+    }
+}
