c34ea83de027c4634fc1179a5fe77ca2980036c4
MisaJovanovic <misaj@nordeus.com>
2020-11-13 23:16:42 +0100

    shader updates

--- client/Assets/Scripts/View/PlanetView.cs
+++ client/Assets/Scripts/View/PlanetView.cs
@@ -21,30 +21,30 @@ namespace View
             var atmosphereHeight = Planet.AtmosphereHeight;
             var surfaceObject = Instantiate(Planet.Settings.surfacePrefab, t);
             surface = surfaceObject.GetComponent<MeshRenderer>();
-            if (surface.materials.Length > 0)
-            {
-                var ocean = surface.materials[0];
-                ocean.SetColor("_Color", Planet.Settings.oceanColor);
-                ocean.SetColor("_AtmosphereTint", Planet.Settings.atmosphereColor);
-                ocean.SetFloat("_AtmosphereAlpha", Planet.Settings.innerAtmosphereDensity);
-                ocean.SetFloat("_AtmosphereHeight", atmosphereHeight);
-            }
-            if (surface.materials.Length > 1)
-            {
-                var terrain = surface.materials[1];
-                terrain.SetColor("_Color", Planet.Settings.terrainColor);
-                terrain.SetColor("_AtmosphereTint", Planet.Settings.atmosphereColor);
-                terrain.SetFloat("_AtmosphereAlpha", Planet.Settings.innerAtmosphereDensity);
-                terrain.SetFloat("_AtmosphereHeight", atmosphereHeight);
-            }
+            //if (surface.materials.Length > 0)
+            //{
+            //    var ocean = surface.materials[0];
+            //    ocean.SetColor("_Color", Planet.Settings.oceanColor);
+            //    ocean.SetColor("_AtmosphereTint", Planet.Settings.atmosphereColor);
+            //    ocean.SetFloat("_AtmosphereAlpha", Planet.Settings.innerAtmosphereDensity);
+            //    ocean.SetFloat("_AtmosphereHeight", atmosphereHeight);
+            //}
+            //if (surface.materials.Length > 1)
+            //{
+            //    var terrain = surface.materials[1];
+            //    terrain.SetColor("_Color", Planet.Settings.terrainColor);
+            //    terrain.SetColor("_AtmosphereTint", Planet.Settings.atmosphereColor);
+            //    terrain.SetFloat("_AtmosphereAlpha", Planet.Settings.innerAtmosphereDensity);
+            //    terrain.SetFloat("_AtmosphereHeight", atmosphereHeight);
+            //}
 
             var atmosphereTransform = atmosphere.transform;
             atmosphereTransform.localScale = Planet.Settings.atmosphereScale * Vector3.one;
 
             var atmosphereMat = atmosphere.material;
-            atmosphereMat.SetColor("_Tint", Planet.Settings.atmosphereColor);
-            atmosphereMat.SetFloat("_FrontAlpha", Planet.Settings.outerAtmosphereDensity);
-            atmosphereMat.SetFloat("_BackAlpha", Planet.Settings.outerAtmosphereDensity);
+            //atmosphereMat.SetColor("_Tint", Planet.Settings.atmosphereColor);
+            //atmosphereMat.SetFloat("_FrontAlpha", Planet.Settings.outerAtmosphereDensity);
+            //atmosphereMat.SetFloat("_BackAlpha", Planet.Settings.outerAtmosphereDensity);
         }
 
         private void Update()
